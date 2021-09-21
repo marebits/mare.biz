2871c2dad9a83e63ed75a9273b7eea52df88851b
CorpulentBrony <corpulent@brony.email>
2021-09-20 23:29:13 -0400

    Update Web3.mjs

--- script/Web3.mjs
+++ script/Web3.mjs
@@ -153,13 +153,13 @@ class Web3 extends self.EventTarget {
 			return;
 		await loadScriptAsync("script/web3.min.js");
 		this.__web3 = new self.Web3(this.provider);
-		console.log(`this.__web3: ${this.__web3}`);
+		console.info({ web3: web3.__web3 });
 		self.Object.defineProperties(this, {
 			eth: { enumerable: true, value: this.__web3.eth }, 
 			utils: { enumerable: true, value: this.__web3.utils }
 		});
-		console.log(`this.eth: ${this.eth}`);
-		console.log(`this.utils: ${this.utils}`);
+		console.info({eth: web3.eth});
+		console.info({utils: web3.utils});
 	}
 	__onAccountsChanged(accounts) {
 		console.log("accounts changed");
