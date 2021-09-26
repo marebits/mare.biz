dcf2e2be92c506d136d4c09c29ae85f4b121bbd4
CorpulentBrony <corpulent@brony.email>
2021-09-25 23:55:28 -0400

    Update ContractLink.mjs

--- script/ContractLink.mjs
+++ script/ContractLink.mjs
@@ -82,7 +82,7 @@ const TEMPLATE = self.document.createElement("template");
 TEMPLATE.innerHTML = `
 	<template>
 		<style id="style">${INLINE_CSS}</style>
-		<a rel="${CONSTANTS.CONTRACT_LINK.REL}" target="${CONSTANTS.CONTRACT_LINK.TARGET}"><slot></slot></a>
+		<a id="a" rel="${CONSTANTS.CONTRACT_LINK.REL}" target="${CONSTANTS.CONTRACT_LINK.TARGET}"><slot></slot></a>
 		<button title="${CONSTANTS.CONTRACT_LINK.BUTTON_TITLE}"></button>
 		<template><output>${CONSTANTS.CONTRACT_LINK.COPY_MESSAGE}</output></template>
 	</template>
@@ -103,10 +103,10 @@ function createCopiedOutputElement() {
 function createDom(options) {
 	const privates = _privates.get(this);
 	const template = TEMPLATE.content.cloneNode(true);
-	privates.anchor = template.querySelector("a");
+	privates.anchor = template.getElementById("a");
 	console.log({ template });
-	console.log({ querySelector: template.querySelector });
-	console.log({ a: template.querySelector("a") });
+	console.log({ querySelector: template.getElementById });
+	console.log({ a: template.getElementById("a") });
 	setAttributes(privates.anchor, { href: this.href, title: this.title });
 	privates.button = template.querySelector("button");
 	privates.copiedOutputElementTemplate = template.querySelector("template").content;
