2d26b0b85464832e6a70e7da7aebf1f49fc7fdfa
CorpulentBrony <corpulent@brony.email>
2021-09-25 23:45:03 -0400

    Update ContractLink.mjs

--- script/ContractLink.mjs
+++ script/ContractLink.mjs
@@ -81,6 +81,7 @@ const INLINE_CSS = `
 const TEMPLATE = self.document.createElement("template");
 TEMPLATE.innerHTML = `
 	<template>
+		<style id="style">${INLINE_CSS}</style>
 		<a rel="${CONSTANTS.CONTRACT_LINK.REL}" target="${CONSTANTS.CONTRACT_LINK.TARGET}"><slot></slot></a>
 		<button title="${CONSTANTS.CONTRACT_LINK.BUTTON_TITLE}"></button>
 		<template><output>${CONSTANTS.CONTRACT_LINK.COPY_MESSAGE}</output></template>
@@ -102,14 +103,13 @@ function createCopiedOutputElement() {
 function createDom(options) {
 	const privates = _privates.get(this);
 	const template = TEMPLATE.content.cloneNode(true);
-	const style = createElement("style", {}, template, INLINE_CSS);
-	console.log(style.sheet);
-	style.sheet.insertRule(`@media screen { button::after { background-image: url("${CONSTANTS.CONTRACT_LINK.BUTTON_IMAGE_TAG(this.buttonColor)}"); } }`, 0);
 	privates.anchor = template.querySelector("a");
 	setAttributes(privates.anchor, { href: this.href, title: this.title });
 	privates.button = template.querySelector("button");
 	privates.copiedOutputElementTemplate = template.querySelector("template").content;
 	this.attachShadow({ mode: "open" }).appendChild(template);
+	const style = template.getElementById("style");
+	style.sheet.insertRule(`@media screen { button::after { background-image: url("${CONSTANTS.CONTRACT_LINK.BUTTON_IMAGE_TAG(this.buttonColor)}"); } }`, 0);
 }
 function getAttributeOrDefault(attribute, alternative) {
 	const value = this.getAttribute(attribute);
