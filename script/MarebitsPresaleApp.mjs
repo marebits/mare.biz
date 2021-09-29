17f5e275f644cd0039bf424cebf5c63606081829
CorpulentBrony <corpulent@brony.email>
2021-09-29 00:17:50 -0400

    Update MarebitsPresaleApp.mjs

--- script/MarebitsPresaleApp.mjs
+++ script/MarebitsPresaleApp.mjs
@@ -45,12 +45,13 @@ class MarebitsPresaleApp extends MareCustomElement {
 				new MareEvent(this.web3, "accountsChanged", updateButtons.bind(this)), 
 				new MareEvent(this.web3, "disconnected", updateButtons.bind(this)), 
 				new MareEvent(this.web3, "initialized", updateButtons.bind(this))
-			])
-		})().then(super.connectedCallback.bind(this)).catch(console.error);
+			]);
+			super.connectedCallback();
+		})().catch(console.error);
 	}
 	createdCallback() {
 		this.web3 = new Web3();
-		// defineCustomElements([MarebitsPresaleSale, MarebitsPresaleStatus]);
+		// defineCustomElements([MarebitsPresaleStatus]);
 		_privates.set(this, {});
 		_privates.get(this).createDomPromise = createDom.call(this).then(super.createdCallback.bind(this)).catch(console.error);
 	}
