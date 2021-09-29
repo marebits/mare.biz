84b2f060487422baca792c63abea9c2a38c8c6e1
CorpulentBrony <corpulent@brony.email>
2021-09-28 23:49:06 -0400

    Update MarebitsPresaleApp.mjs

--- script/MarebitsPresaleApp.mjs
+++ script/MarebitsPresaleApp.mjs
@@ -2,28 +2,21 @@
 	static content, including contract addresses, etc.
 		-- include button to add MARE token to wallet
 
-	sale status (is it open, is it finalized, when it opens, when it closes, how much MARE sold, how much ETH raised)
-		The pre-sale is currently closed, but is scheduled to open in X hours/minutes/countdown
-		The pre-sale is open!  Currently sold X MARE out of a total of Y. <progress>  Pre-sale closes in X hours/minutes/countdown
-		The pre-sale is closed, we sold X MARE out of a total of Y. <progress> Be sure to withdraw your MARE if you purchased any!
+	sale status
 	
 	sale area
-		when no wallet is connected: Show connect wallet button
-		when wallet is connected: show wallet address and current pending MARE balance
-
-		purchase area -- only visible if wallet is connected and sale is open
-			purchase input field, purchase button, field showing how much MARE you get
-		withdraw area - only visible if wallet is connected and sale is closed and user has >0 MARE to withdraw
-			withdraw button
 */
 
+import { browserEvents } from "./EventSet.mjs";
+// import { MarebitsPresaleSale } from "./MarebitsPresaleSale.mjs";
+// import { MarebitsPresaleStatus } from "./MarebitsPresaleStatus.mjs";
 import { MareCustomElement } from "./MareCustomElement.mjs";
-import { fetchHtml, preload } from "./utils.mjs";
+import { MareEvent } from "./MareEvent.mjs";
+import { defineCustomElements, fetchHtml, preload } from "./utils.mjs";
+import { Web3 } from "./Web3.mjs";
 
 // configurable constants 
 const TAG_NAME = "marebits-presale-app";
-
-// CSS
 
 // HTML
 const HTML_FILE = "presale/index.html";
@@ -35,16 +28,34 @@ const _privates = new self.WeakMap();
 async function createDom() {
 	const privates = _privates.get(this);
 	const doc = await fetchHtml(HTML_FILE);
-	console.log(doc);
+	const template = doc.body.querySelector("template").content.cloneNode(true);
+	privates.elements = { buttons: { addToMetamask: template.getElementById("add-to-metamask") } };
+	this.attachShadow({ mode: "open" }).appendChild(template);
 }
+function onClickAddToMetamask() { this.web3.mare.watchAsset().catch(console.error); }
+function updateButtons() { (async function() { addToMetaMaskButton.disabled = !(this.web3.isConnected && await this.web3.isTargetChain); })().catch(console.error); }
 
 class MarebitsPresaleApp extends MareCustomElement {
-	connectedCallback() {}
-	createdCallback() {
-		_privates.set(this, {});
-		createDom.call(this).catch(console.error);
+	connectedCallback() {
+		const privates = _privates.get(this);
+		browserEvents.addMany(privates.events = [
+			new MareEvent(privates.elements.buttons.addToMetamask, "click", onClickAddToMetamask.bind(this), { passive: true }), 
+			new MareEvent(this.web3, "accountsChanged", updateButtons.bind(this)), 
+			new MareEvent(this.web3, "disconnected", updateButtons.bind(this)), 
+			new MareEvent(this.web3, "initialized", updateButtons.bind(this))
+		]);
+		super.connectedCallback();
 	}
-	disconnectedCallback() {}
+	createdCallback() {
+		this.web3 = new Web3();
+		_privates.set(this, {});
+		// defineCustomElements([MarebitsPresaleSale, MarebitsPresaleStatus]);
+		createDom.call(this).then(super.createdCallback).catch(console.error);
+	}
+	disconnectedCallback() {
+		browserEvents.deleteMany(_privates.get(this).events);
+		super.disconnectedCallback();
+	}
 }
 MarebitsPresaleApp.TAG_NAME = TAG_NAME;
 
