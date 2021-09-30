4afcdeebd4558697931c7dd7042afac401cefafd
CorpulentBrony <corpulent@brony.email>
2021-09-30 16:46:55 -0400

    Added reduce method to BetterMap

--- script/BetterMap.mjs
+++ script/BetterMap.mjs
@@ -15,6 +15,29 @@ class BetterMap extends self.Map {
 			}
 		});
 	}
+	reduce(callbackFn, initialValue, thisArg = undefined) {
+		const len = this.length;
+
+		if (typeof callbackFn === "undefined" || typeof callbackFn !== "function" || typeof callbackFn.call !== "function")
+			return initialValue;
+		else if (len === 0 && typeof initialValue === "undefined")
+			return;
+		const iterator = this[self.Symbol.iterator]();
+		let accumulator = undefined;
+		let current = {};
+		let isDone = false;
+
+		if (typeof initialValue !== "undefined")
+			accumulator = initialValue;
+		else {
+			current = iterator.next();
+			accumulator = current.value[1];
+		}
+
+		while (!(current = iterator.next()).done)
+			accumulator = callbackFn.call(thisArg, accumulator, current.value[1], current.value[0], this);
+		return accumulator;
+	}
 }
 
 export { BetterMap };
