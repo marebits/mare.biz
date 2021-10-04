3c5402f02f9c1ada76cac39d6ae5ec97135823ca
CorpulentBrony <corpulent@brony.email>
2021-10-04 16:49:20 -0400

    Update MarebitsPresaleStatus.mjs

--- script/MarebitsPresaleStatus.mjs
+++ script/MarebitsPresaleStatus.mjs
@@ -9,33 +9,70 @@
 	emitted events: 
 */
 
-import { Cache } from "./Cache.mjs";
+import { BetterMap } from "./BetterMap.mjs";
 import { CONSTANTS } from "./constants.mjs";
 import { browserEvents } from "./EventSet.mjs";
 // import { MareCountDownClock } from "./MareCountDownClock.mjs";
 import { MareCustomElement } from "./MareCustomElement.mjs";
 import { MareEvent } from "./MareEvent.mjs";
 import { OutputDataMessage } from "./OutputDataMessage.mjs";
-import { VisibilityListener, defineCustomElements } from "./utils.mjs";
+import { VisibilityListener, defineCustomElements, hyphenToCamelCase } from "./utils.mjs";
 
 // configurable constants 
 const TAG_NAME = "marebits-presale-status";
 
 // other constants (not configurable)
 const _privates = new self.WeakMap();
-const CACHE_PREFIX = `${CONSTANTS.PRESALE.CONTRACT_ADDRESS}.`;
+
+// helper classes
+class MareCommonElements extends self.Set {
+	set value(value) { this.forEach(element => element.value = value); }
+	replaceChildren(...nodes) { this.forEach(element => element.replaceChildren(...nodes)); }
+	setAttribute(name, value) { this.forEach(element => element.setAttribute(name.toString(), value); }
+}
+class MareStatusSection {
+	constructor(element) { _privates.set(this, { element }); }
+	hide() {
+		const element = _privates.get(this).element;
+		element.setAttribute("aria-hidden", element.hidden = true);
+	}
+	show() {
+		const element = _privates.get(this).element;
+		element.hidden = false;
+		element.removeAttribute("aria-hidden");
+	}
+}
+class MareStatusSectionCollection extends BetterMap {
+	constructor(doc, ...elementIds) { super(elementIds.map(elementId => [hyphenToCamelCase(elementId), new MareStatusSection(doc.getElementById(elementId))])); }
+	hide() { this.forEach(element => element.hide()); }
+	show() { this.forEach(element => element.show()); }
+}
 
 // private methods
-function cacheKey(key) { return `${CACHE_PREFIX}.${key.toString()}`; }
 function createDom() {
 	const privates = _privates.get(this);
 	const template = this.app.shadowRoot.getElementById("marebits-presale-status").content.cloneNode(true);
 	privates.elements = {
+		clocks: {
+			closing: template.getElementById("closing"), 
+			opening: template.getElementById("opening")
+		}, 
+		meters: new MareCommonElements(template.getElementsByClassName("amount-sold")), 
 		outputs: {
-			ethRaised: template.getElementById("eth-raised"), 
-			mareSold: template.getElementById("marebits-sold")
-		}
+			available: new MareCommonElements(template.getElementsByClassName("available")), 
+			chainName: template.getElementById("chain-name"), 
+			sold: new MareCommonElements(template.getElementsByClassName("sold"))
+		}, 
+		statuses: new MareStatusSectionCollection(template, "closed", "need-permission", "not-yet-open", "no-wallet", "open", "wrong-chain")
 	};
+	privates.elements.outputs.chainName.replaceChildren(self.document.createTextNode(CONSTANTS.CHAINS.get(CONSTANTS.TARGET_CHAIN_ID).name));
+	const meterMax = CONSTANTS.PRESALE.CAP * self.Number(CONSTANTS.TOKEN.SALE_RATE);
+	privates.elements.meters.setAttribute("max", meterMax);
+	privates.elements.meters.setAttribute("low", self.Math.floor(meterMax * 0.33));
+	privates.elements.meters.setAttribute("high", self.Math.floor(meterMax * 0.75));
+	privates.elements.meters.setAttribute("optimum", self.Math.floor(meterMax * 0.9));
+	privates.elements.outputs.available.replaceChildren(self.document.createTextNode(meterMax.toString()));
+	privates.elements.outputs.available.value = meterMax;
 	this.attachShadow({ mode: "open" }).appendChild(template);
 }
 function onVisibilityChange() {
@@ -47,38 +84,39 @@ function updateStatus() {
 		const currentAccount = this.app.web3.currentAccount;
 		const isTargetChain = this.app.web3.isTargetChain;
 		const privates = _privates.get(this);
-		privates.elements.outputs.ethRaised.value = privates.elements.outputs.mareSold.value = "0";
+		privates.elements.statuses.hide();
 
 		if (this.app.web3.isConnected && typeof await currentAccount !== "undefined" && await isTargetChain) {
-			let hasClosed = Cache.Typed.persisted.get(cacheKey`hasClosed`);
-			let isOpen;
-
-			if (hasClosed)
-				isOpen = false;
-			else if (hasClosed = await this.app.web3.mare.hasClosed) {
-				isOpen = false;
-				Cache.Typed.persisted.set(cacheKey`hasClosed`, true);
-			} else
-				isOpen = await this.app.web3.mare.isOpen;
+			const hasClosed = await this.app.web3.mare.hasClosed;
+			const isOpen = hasClosed ? false : await this.app.web3.mare.isOpen;
 
 			if (isOpen) {
 				// The pre-sale is open!  Currently sold X MARE out of a total of Y. <progress>  Pre-sale closes in X hours/minutes/countdown
-				const [closingTime, ethRaised, mareSold] = await self.Promise.all([
-					self.Promise.resolve(Cache.Typed.persisted.getOrSet(cacheKey`closingTime`, () => this.app.web3.mare.closingTime.then(self.Number.parseInt))), 
-					(async () => privates.elements.outputs.ethRaised.value = await this.app.web3.mare.ethRaised)(), 
-					(async () => privates.elements.outputs.mareSold.value = await this.app.web3.mare.mareSold)()
+				await self.Promise.all([
+					(async () => privates.elements.clocks.closing.endTime = await this.app.web3.mare.closingTime * 1000)(), 
+					(async () => privates.elements.meters.value = privates.elements.outputs.sold.value = await this.app.web3.mare.mareSold)()
 				]);
+				privates.elements.statuses.open.show();
 			} else if (hasClosed) {
 				// The pre-sale is closed, we sold X MARE out of a total of Y. <progress> Be sure to withdraw your MARE if you purchased any!
-				const [ethRaised, mareSold] = await self.Promise.all([
-					self.Promise.resolve(Cache.Typed.persisted.getOrSet(cacheKey`ethRaised`, () => this.app.web3.mare.ethRaised)), 
-					self.Promise.resolve(Cache.Typed.persisted.getOrSet(cacheKey`mareSold`, () => this.app.web3.mare.mareSold))
-				]);
+				privates.elements.meters.value = privates.elements.outputs.sold.value = await this.app.web3.mare.mareSold;
+				privates.elements.statuses.closed.show();
 			} else {
 				// The pre-sale is currently closed, but is scheduled to open in X hours/minutes/countdown
-				const openingTime = await self.Promise.resolve(Cache.Typed.persisted.getOrSet(cacheKey`openingTime`, () => this.app.web3.mare.openingTime.then(self.Number.parseInt)));
+				privates.elements.clocks.opening.endTime = await this.app.web3.mare.openingTime * 1000;
+				privates.elements.statuses.notYetOpen.show();
 			}
-		}
+		// } else if (!this.app.web3.isConnected) {
+			// no web3 wallet found, page will need to be reloaded or wallet connected
+			// privates.elements.statuses.noWallet.show();
+		} else if (!(await isTargetChain)) {
+			// wrong chain
+			privates.elements.statuses.wrongChain.show();
+		} else if (typeof await currentAccount === "undefined") {
+			// wallet detected and connected, but we need permission to use it
+			privates.elements.statuses.needPermission.show();
+		} else
+			privates.elements.statuses.noWallet.show();
 	})().catch(console.error);
 }
 
@@ -95,6 +133,8 @@ class MarebitsPresaleStatus extends MareCustomElement {
 	}
 
 	connectedCallback() {
+		if (!this.isConnected)
+			return;
 		const privates = _privates.get(this);
 		browserEvents.addMany(privates.events = [
 			new MareEvent(this.app.web3, "accountsChanged", updateStatus.bind(this)), 
